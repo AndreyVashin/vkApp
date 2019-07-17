594ad6d9d31b9ab3716585246133e3a901bf63d9
Андрей Вашин <andreyvashin@gmail.com>
2019-07-17 10:12:22 +0700

    Updates

---
+++ precache-manifest.17d3dde98101cab282273c13b8fc16f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee13918ca33b968b2110",
+    "url": "/static/js/main.4c1ed9f4.chunk.js"
+  },
+  {
+    "revision": "9f70292e5436d2f2ca16",
+    "url": "/static/js/2.0a778a57.chunk.js"
+  },
+  {
+    "revision": "ee13918ca33b968b2110",
+    "url": "/static/css/main.3e2b10af.chunk.css"
+  },
+  {
+    "revision": "9f70292e5436d2f2ca16",
+    "url": "/static/css/2.bff2f301.chunk.css"
+  },
+  {
+    "revision": "29e9e19757e3f8a362d824b2aa6e114c",
+    "url": "/index.html"
+  }
+];
